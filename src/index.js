bed5f4028777ebb8ddd4c9c4be2e4489e85a3552
Dennis <dennis940126@gmail.com>
2021-02-10 19:35:52 +0800

    production setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const path = require("path");
 const app = express();
 
 const publicDirPath = path.join(__dirname, "../public");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicDirPath, "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
