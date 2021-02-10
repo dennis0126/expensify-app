2d340c10a71080cfc8e95920a41b052a31af7c98
Dennis <dennis940126@gmail.com>
2021-02-10 19:00:27 +0800

    setup webpack for production

--- src/app.js
+++ src/app.js
@@ -11,16 +11,6 @@ const appRoot = document.getElementById("app");
 
 const store = configureStore();
 
-import { addExpense } from "./actions/expenses";
-import { setTextFilter, sortByAmount } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
-
-store.dispatch(addExpense({ title: "Water bill", amount: 500, createdAt: 1 }));
-store.dispatch(addExpense({ title: "Gas bill", amount: 400, createdAt: 5 }));
-store.dispatch(addExpense({ title: "Rent", amount: 1300, createdAt: 3 }));
-// store.dispatch(setTextFilter("Gas"));
-store.dispatch(sortByAmount());
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/app.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    // new CleanWebpackPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: "Production",
+    // }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
